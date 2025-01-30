4b291d5ed3c39879a07d4a5c57b8b7b4a6a38d2d
Vignesh <vignesh.g@tringapps.com>
2025-01-30 17:09:05 +0530

    Deposit,Withdraw Amount Validation Added

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,7 @@
 //This Class Implement Bank Account
+
 class BankAccount {
+
     private int accountNumber;
     private String name;
     private double balance;
@@ -11,29 +13,40 @@ class BankAccount {
         this.balance = balance;
         this.atmPin = atmPin;
     }
+
     //Method to deposit money
     public void deposit(double amount) {
-        this.balance += amount;
-        System.out.println(amount + " deposited in " + accountNumber + " account");
+        if (amount < 0) {
+            System.out.println("Amount cannot be negative");
+        } else {
+            this.balance += amount;
+            System.out.println(amount + " deposited in " + accountNumber + " account");
+        }
     }
+
     //Method to withdraw
     public void withdraw(double amount) {
-        if (this.balance < amount) {
+        if (amount < 0) {
+            System.out.println("Amount cannot be negative");
+        } else if (this.balance < amount) {
             System.out.println("Insufficient Balance!!!.Your Balance is :" + this.balance + " You cannot withdraw " + amount);
         } else {
             this.balance -= amount;
         }
     }
+
     //Method to display balance
     public void displayBalance() {
         System.out.println("Dear " + this.name + ",");
         System.out.println("Your Account number is : " + accountNumber);
         System.out.println("Your Account Balance is : " + balance);
     }
+
     //Getters
     public String getName() {
         return name;
     }
+
     public int getAccountNumber() {
         return accountNumber;
     }
