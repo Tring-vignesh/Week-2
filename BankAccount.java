f09c42ce3d8cad428f9e075cba6697aaf25d1a26
Tring-vignesh <vignesh.g@tringapps.com>
2025-01-30 15:39:34 +0530

    Task 1 Atm simulation is added

---
+++ BankAccount.java
@@ -0,0 +1,44 @@
+//This Class Implement Bank Account
+class BankAccount {
+    private int accountNumber;
+    private String name;
+    private double balance;
+    private int atmPin;
+
+    public BankAccount(int accountNumber, String name, double balance, int atmPin) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.balance = balance;
+        this.atmPin = atmPin;
+    }
+    //Method to deposit money
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.println(amount + " deposited in " + accountNumber + " account");
+    }
+    //Method to withdraw
+    public void withdraw(double amount) {
+        if (this.balance < amount) {
+            System.out.println("Insufficient Balance!!!.Your Balance is :" + this.balance + " You cannot withdraw " + amount);
+        } else {
+            this.balance -= amount;
+        }
+    }
+    //Method to display balance
+    public void displayBalance() {
+        System.out.println("Dear " + this.name + ",");
+        System.out.println("Your Account number is : " + accountNumber);
+        System.out.println("Your Account Balance is : " + balance);
+    }
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getAtmPin() {
+        return atmPin;
+    }
+}
